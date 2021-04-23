3bc3e1a9907e085dca34163d2e1d6259f2c0b366
MurrayWarnock <murray.warnock@gmail.com>
2021-04-23 08:19:14 -0600

    App.js set up component Order.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
 import React from "react";
+// import ReactDOM from "react-dom";
+import { Route, BrowserRouter as Router, useHistory, useRouteMatch } from 'react-router-dom';
+import Order from './components/Order';
+// import App from "./App";
 
 const App = () => {
+
+  
   return (
-    <>
-      <h1>Lambda Eats</h1>
-      <p>You can remove this code and create your own header</p>
+  
+    <><header class="app-header">
+        <h1>Lambda Eats</h1>
+        <p>You can remove this code and create your own header</p>
+        <button>Pizza?</button>
+      </header>
+      <Order />
+
+      <Route exact path="/" component={App}/>
+      <Route path="/order" component={Order} />
+
+
     </>
   );
 };
